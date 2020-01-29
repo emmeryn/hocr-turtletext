c5e449b0fc69e2a71d390dfc248cacf52aff951c
Sue Zheng Hao <szhlibrary@gmail.com>
2020-01-29 23:40:35 +0800

    Reader: Process each ocr_word independently of the ocr_line that it belongs to. Don't assume ocr_line from hOCR output is a line. Resolves bugs where words that were visually on the same line but belonged to different ocr_line would not be concatenated properly by the x_whitespace_threshold.

--- lib/hocr_turtletext/reader.rb
+++ lib/hocr_turtletext/reader.rb
@@ -10,12 +10,14 @@ class HocrTurtletext::Reader
 
   def content
     hocr_content = File.read(@hocr_path)
-    lines = precise_content(hocr_content)
-    pos_hash = to_pos_hash(lines)
-    fuzzed_y(pos_hash)
+    html = Nokogiri::HTML(hocr_content)
+    pos_info_words = extract_words_from_html(html)
+    pos_hash = to_pos_hash pos_info_words
+    fuzzed_y = fuzzed_y(pos_hash)
+    concat_words_in_lines(fuzzed_y)
   end
 
-  def text_in_region(xmin,xmax,ymin,ymax,inclusive=false)
+  def text_in_region(xmin, xmax, ymin, ymax, inclusive=false)
     return [] unless xmin && xmax && ymin && ymax
     text_map = content
     box = []
@@ -37,12 +39,12 @@ class HocrTurtletext::Reader
   def text_position(text)
     item = if text.class <= Regexp
              content.map do |k,v|
-               if x = v.reduce(nil){|memo,vv|  memo = (vv[1] =~ text) ? vv[0] : memo  }
+               if x = v.reduce(nil){ |memo,vv|  memo = (vv[1] =~ text) ? vv[0] : memo }
                  [k,x]
                end
              end
            else
-             content.map {|k,v| if x = v.rassoc(text) ; [k,x] ; end }
+             content.map { |k,v| if x = v.rassoc(text) ; [k,x] ; end }
            end
     item = item.compact.flatten
     unless item.empty?
@@ -51,7 +53,7 @@ class HocrTurtletext::Reader
   end
 
   def bounding_box(&block)
-    HocrTurtletext::Textangle.new(self,&block)
+    HocrTurtletext::Textangle.new(self, &block)
   end
 
   private
@@ -67,7 +69,9 @@ class HocrTurtletext::Reader
   def fuzzed_y(input)
     output = []
     input.keys.sort.each do |precise_y|
-      matching_y = output.map(&:first).select { |new_y| (new_y - precise_y).abs < y_precision }.first || precise_y
+      matching_y = output.map(&:first)
+                         .select { |new_y| (new_y - precise_y).abs < y_precision }
+                         .first || precise_y
       y_index = output.index{ |y| y.first == matching_y }
       new_row_content = input[precise_y].to_a
       if y_index
@@ -81,56 +85,39 @@ class HocrTurtletext::Reader
     output
   end
 
-  def precise_content(hocr_content)
-    html = Nokogiri::HTML(hocr_content)
-    lines = []
-    html.css('span.ocr_line').map do |line|
-      chunks = chunks_from_processed_ocr_line(line)
-      lines.concat(chunks)
-    end
-    lines
-  end
-
-  def chunks_from_processed_ocr_line(ocr_line)
-    pos_info_line = add_positional_info_to_line(ocr_line)
-    sorted_pos_info_line = sort_words_in_line(pos_info_line)
-    concat_words_in_line(sorted_pos_info_line)
-  end
-
-  def add_positional_info_to_line(ocr_line)
-    ocr_line.css('span.ocrx_word, span.ocr_word')
-            .reject { |word| word.text.strip.empty? }
-            .map do |word|
-      word_attributes = word.attributes['title'].value.to_s
-                            .delete(';').split(' ')
-      info(word, word_attributes)
-    end
-  end
-
-  def sort_words_in_line(pos_info_line)
-    # sort word by x value, concat if x2.x_start - x1.x_end < some_x_threshold
-    pos_info_line.sort_by { |word| word[:x_start] }
-    pos_info_line.slice_when do |x, y|
-      y[:x_start] - x[:x_end] > x_whitespace_threshold
-    end.to_a
-  end
-
-  def concat_words_in_line(sorted_pos_info_line)
-    chunks = []
-    # merge all words in each chunk
-    sorted_pos_info_line.each do |chunk|
-      sentence = nil
-      chunk.each do |word|
-        if sentence.nil?
-          sentence = word
+  def concat_words_in_lines(fuzzed_y)
+    fuzzed_y.map do |line|
+      x_pos_keyed_words = line[1]
+      concatenated_words = []
+      x_pos_keyed_words.each do |x_pos_keyed_word|
+        word_hash = x_pos_keyed_word[1]
+        if concatenated_words.empty? ||
+           word_hash[:x_start] - concatenated_words.last[:x_end] > x_whitespace_threshold
+          concatenated_words.push word_hash
         else
-          sentence[:word] = "#{sentence[:word]} #{word[:word]}"
-          sentence[:x_end] = word[:x_end]
+          concatenated_words.last[:word] = "#{concatenated_words.last[:word]} #{word_hash[:word]}"
+          concatenated_words.last[:x_end] = word_hash[:x_end]
         end
       end
-      chunks.push sentence
+      line[1] = concatenated_words.map! do |word_hash|
+        [word_hash[:x_start], word_hash[:word]]
+      end
+      line
     end
-    chunks
+  end
+
+  def extract_words_from_html(html)
+    pos_info_words = []
+
+    html.css('span.ocrx_word, span.ocr_word')
+        .reject { |word| word.text.strip.empty? }
+        .each do |word|
+      word_attributes = word.attributes['title'].value.to_s
+                            .delete(';').split(' ')
+      pos_info_word = word_info(word, word_attributes)
+      pos_info_words.push pos_info_word
+    end
+    pos_info_words
   end
 
   def to_pos_hash(lines)
@@ -139,19 +126,18 @@ class HocrTurtletext::Reader
     pos_hash = {}
     lines.each do |run|
       pos_hash[run[:y_start]] ||= {}
-      pos_hash[run[:y_start]][run[:x_start]] ||= ''
-      pos_hash[run[:y_start]][run[:x_start]] << run[:word]
+      pos_hash[run[:y_start]][run[:x_start]] = run
     end
     pos_hash
   end
 
-  def info(word, data)
+  def word_info(word, data)
     {
-        word: word.text,
-        x_start: data[1].to_i,
-        y_start: data[2].to_i,
-        x_end: data[3].to_i,
-        y_end: data[4].to_i
+      word: word.text,
+      x_start: data[1].to_i,
+      y_start: data[2].to_i,
+      x_end: data[3].to_i,
+      y_end: data[4].to_i
     }
   end
 end
